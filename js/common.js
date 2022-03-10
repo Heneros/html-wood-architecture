a156d291e709ade78f041c4df4a73465b5f7a4b1
Heneros <punk60826@gmail.com>
2022-03-11 01:57:58 +0200

    added scroll to home-slider

--- js/common.js
+++ js/common.js
@@ -25,7 +25,20 @@ document.addEventListener('DOMContentLoaded', function(){
     const homeSlider = new Swiper('.home-slider', {
         speed: 800,
         effect: 'fade',
-        centeredSlides: true
-    });
+        centeredSlides: true,
+        pagination: {
+			el: '.home-slider__pagination',
+			type: 'custom',
+			renderCustom: function(swiper, current, total) {
+				let indT = total >= 10 ? total : `0${total}`
+				let indC = current >= 10 ? current : `0${current}`
+				return `<b>${indC}</b><span></span> ${indT}`
+			}
+		},
+        scrollbar: {
+            el: '.home-slider__scrollbar',
+            draggable: true
+        },
+    })
 
 });
