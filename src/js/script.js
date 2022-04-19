2ac66f096f13021e9c20efcde4d986e7045d2dbf
Heneros <punk60826@gmail.com>
2022-04-19 23:59:49 +0300

    tabs styled

--- src/js/script.js
+++ src/js/script.js
@@ -139,4 +139,14 @@ document.addEventListener('DOMContentLoaded', function(){
           swiper: swiper,
         },
       });
+
+
+      function openTab(cityName) {
+        var i;
+        var x = document.getElementsByClassName("tab");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";  
+        }
+        document.getElementById(cityName).style.display = "block";  
+      }
 });
